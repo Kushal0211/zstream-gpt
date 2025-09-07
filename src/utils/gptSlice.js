023eadc74a2c165151b72889717627f454198acb
Kushal Pandita <kushalpandita007@gmail.com>
2025-09-08 01:25:54 +0530

    gpt added

--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -4,13 +4,21 @@ const gptSlice = createSlice({
     name : 'gpt',
     initialState : {
         currentState : false,
+        movieResults : null,
+        movieNames : null,
     },
     reducers : {
         toggleSearchView (state,action) {
             state.currentState = !state.currentState
+        },
+
+        addGptMoviesToTheStore (state, action) {
+            const { movieNames , movieResults} = action.payload;
+            state.movieResults = movieResults;
+            state.movieNames = movieNames;
         }
     }
 })
 
-export  const {toggleSearchView} = gptSlice.actions;
+export  const {toggleSearchView, addGptMoviesToTheStore} = gptSlice.actions;
 export default gptSlice.reducer;
